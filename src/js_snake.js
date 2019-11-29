0a8bbfab3587e6d70d14e2ce0a7c27988e5a1a8b
Matt Purdy <matt9986@gmail.com>
2019-11-29 14:56:09 -0800

    Create build that doesn't automatically pop up the game

---
+++ src/js_snake.js
@@ -0,0 +1,9 @@
+var JsSnake = {
+    SnakeGame: require('./snake_game'),
+    CanvasDisplay: require('./canvas_display'),
+    FaviconDisplay: require('./favicon_display')
+};
+
+module.exports = JsSnake;
+
+global.JsSnake = JsSnake;
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   mode: "production",
   entry: {
     canvas_game: './src/start_canvas.js',
-    favicon_game: './src/start_favicon.js'
+    favicon_game: './src/start_favicon.js',
+    js_snake: './src/js_snake.js'
   },
   module: {
     rules: [
