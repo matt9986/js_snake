046bd9cea7c6120a63ea3c54b7800be5cf80458c
Matt Purdy <matt9986@gmail.com>
2023-02-20 15:57:19 -0800

    Movement refactors

--- src/movement.js
+++ src/movement.js
@@ -1,8 +1,8 @@
-const move_point = change => segment => [segment[0] + change[0], segment[1] + change[1]];
+const move_point = (change, segment) => [segment[0] + change[0], segment[1] + change[1]];
 const move_snake = (snake, change) => {
     let prev_point = null;
     return snake.map(segment => {
-        let new_val = prev_point == null ? move_point(change)(segment) : prev_point;
+        let new_val = prev_point == null ? move_point(change, segment) : prev_point;
         prev_point = segment;
         return new_val;
     })
@@ -11,5 +11,3 @@ export const up = snake => move_snake(snake, [0, 1]);
 export const down = snake => move_snake(snake, [0, -1]);
 export const right = snake => move_snake(snake, [1, 0]);
 export const left = snake => move_snake(snake, [-1, 0]);
-
-export default {up, down, right, left}
